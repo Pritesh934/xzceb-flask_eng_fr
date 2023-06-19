92b019325dbd85026cbe194ffc13619d1a9d9533
Pritesh934 <daspritesh15@gmail.com>
2023-06-19 03:02:27 -0400

    Final changes

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,32 @@
+import unittest
+from deep_translator import MyMemoryTranslator
+from translator import english_to_french, french_to_english
+
+
+class testingEnglishtoFrench(unittest.TestCase):
+    def test_hello_translation(self):
+        english_text = "Hello"
+        translated_text = english_to_french(english_text)
+        self.assertEqual(translated_text, "Pepitoooo")
+
+    def test_translation_not_equal(self):
+        english_text = "Hello"
+        translated_text = english_to_french(english_text)
+        self.assertNotEqual(translated_text, "Hola")
+
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test_bonjour_translation(self):
+        french_text = "Bonjour"
+        translated_text = french_to_english(french_text)
+        self.assertEqual(translated_text, "Hello")
+
+    def test_translation_not_equal(self):
+        french_text = "Bonjour"
+        translated_text = french_to_english(french_text)
+        self.assertNotEqual(translated_text, "Bonjour")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,20 @@
+"""
+This module provides functions for translating text from English to French and from French to English.
+"""
+
+from deep_translator import MyMemoryTranslator
+
+def english_to_french(english_text):
+    """
+    Translates English text to French.
+    """
+    translated_text = MyMemoryTranslator(source='en', target='fr').translate(english_text)
+    return translated_text
+
+def french_to_english(french_text):
+    """
+    Translates French text to English.
+    """
+    translated_text = MyMemoryTranslator(source='fr', target='en').translate(french_text)
+    return translated_text
+
